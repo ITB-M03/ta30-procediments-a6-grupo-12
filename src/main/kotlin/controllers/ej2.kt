de6d1d4c53ae6fb5084a833f029f822ec4a51092
sergio.navas.7e8 <sergio.navas.7e8@itb.cat>
2024-12-09 12:29:28 +0100

    ej2 mitad

---
+++ src/main/kotlin/controllers/ej2.kt
@@ -0,0 +1,16 @@
+package org.example.controllers
+import java.util.*
+
+import java.util.Scanner
+
+fun main() {
+    val scanner= Scanner(System.`in`)
+    val matriu = Array(20){Array(15) {"_"} }
+
+    println()
+
+}
+
+fun mostrarMenu(scanner: Scanner){
+    println()
+}
